f3f8a9afe953112a7106ce8abb40a87f8138130e
HashRaygoza <raygoza.gomez@gmail.com>
2020-10-03 17:30:48 -0500

    v1.4
    * Ejemplo completo

--- src/main/java/mx/ticom/poolconexiones/C3P0Ejemplo.java
+++ src/main/java/mx/ticom/poolconexiones/C3P0Ejemplo.java
@@ -11,6 +11,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class C3P0Ejemplo {
@@ -19,53 +20,48 @@ public class C3P0Ejemplo {
 
     public static void main(String[] args) {
         try {
-            // Obtenemos el pool de conexiones
+
             PoolC3P0 pool = PoolC3P0.getInstance();
 
             for (int i = 0; i < 10; i++) {
-                // Solicitamos una conexion al pool
-                Connection cx = pool.getConnection();
-                // Creamos nuestro objecto de consulta
-                Statement consulta = cx.createStatement();
-                // La consulta que ejecutaremos
-                String sql = "SELECT * FROM pruebas.prueba";
-                // Obtenemos los datos
-                ResultSet data = consulta.executeQuery(sql);
 
-                if (data.next() == true) {
+                try ( Connection cx = pool.getConnection()) {
+                    Statement consulta = cx.createStatement();
 
-                    Integer puerto = data.getInt("puerto_serial");
-                    Integer impresora = data.getInt("puerto_impresora");
-                    Integer red = data.getInt("puerto_red");
-                    Integer scaner = data.getInt("puerto_escaner");
+                    String sql = "SELECT * FROM pruebas.prueba";
 
-                    // Los presentamos en pantalla
-                    System.out.println("Vuelta numero " + i);
-                    System.out.println("Puerto " + puerto);
-                    System.out.println("Impresora " + impresora);
-                    System.out.println("Red " + red);
-                    System.out.println("Escaner " + scaner);
-                    System.out.println("");
-                    System.out.println("");
+                    ResultSet data = consulta.executeQuery(sql);
+                    if (data.next() == true) {
+                        Integer puerto = data.getInt("puerto_serial");
+                        Integer impresora = data.getInt("puerto_impresora");
+                        Integer red = data.getInt("puerto_red");
+                        Integer scaner = data.getInt("puerto_escaner");
+
+                        System.out.println("Vuelta numero " + i);
+                        System.out.println("Puerto " + puerto);
+                        System.out.println("Impresora " + impresora);
+                        System.out.println("Red " + red);
+                        System.out.println("Escaner " + scaner);
+                        System.out.println("");
+                        System.out.println("");
+                    }
                 }
 
-                // Cerramos la conexion, esto es vital, de no hacerlo el pool creara una nueva conexion
-                // pero al cerrar liberamos esa conexion para que el pool la reuse
-                cx.close();
-
-                // Un retrazo de 10 segundos para poder ver las conexiones en MySQL Workbench
                 Thread.sleep(10000);
             }
 
         } catch (IOException ex) {
-            System.out.println("Error de entrada salida");
+            LOGGER.log(Level.SEVERE, "Error de entrada/salida");
+            LOGGER.log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
-            System.out.println("Error de conexion a base de datos");
-            ex.printStackTrace();
-        } catch (PropertyVetoException ex) {
-            System.out.println("Error de propiedades");
+            LOGGER.log(Level.SEVERE, "Error de conexion a base de datos");
+            LOGGER.log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
-            System.out.println("Error de interrupcion de proceso");
+            LOGGER.log(Level.SEVERE, "Error de interrupcion de proceso");
+            LOGGER.log(Level.SEVERE, null, ex);
+        } catch (PropertyVetoException ex) {
+            LOGGER.log(Level.SEVERE, "Error de propiedades");
+            LOGGER.log(Level.SEVERE, null, ex);
         }
     }
 }
